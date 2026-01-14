3dcd3b210adcf14632f033c12d3ba5febc9d2d4f
martinalejandroo25 <martinalejandroo25@gmail.com>
2026-01-14 18:08:18 +0100

    Primer Commit prueba de funcionamiento

---
+++ src/main/java/com/jaroso/apiejemplo2026/ApiEjemplo2026Application.java
@@ -0,0 +1,13 @@
+package com.jaroso.apiejemplo2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEjemplo2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEjemplo2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaroso/apiejemplo2026/ApiEjemplo2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaroso.apiejemplo2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEjemplo2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
