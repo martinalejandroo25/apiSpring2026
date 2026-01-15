9171aebf2c1ba632bbe7ec4e4ace6c5d79facc1d
martinalejandroo25 <martinalejandroo25@gmail.com>
2026-01-15 20:37:13 +0100

    Finalizada APIs para Eliminacion, modificacion y creacion

--- src/main/java/com/jaroso/apiejemplo2026/controllers/TaskController.java
+++ src/main/java/com/jaroso/apiejemplo2026/controllers/TaskController.java
@@ -3,11 +3,9 @@ package com.jaroso.apiejemplo2026.controllers;
 import com.jaroso.apiejemplo2026.entities.Task;
 import com.jaroso.apiejemplo2026.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +16,7 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+
     @GetMapping("/tasks")
     public ResponseEntity<List<Task>> getAllTasks(){
         return ResponseEntity.ok(taskService.findAll());
@@ -27,12 +26,43 @@ public class TaskController {
     public ResponseEntity<Task> getTaskById(@PathVariable Long id){
         Optional<Task> task = taskService.findById(id);
         return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-        /*realizaría lo mismo que:
-        if(task.isEmpty())
+    }
+
+    @GetMapping("/tasks/title/{title}")
+    public ResponseEntity<Task> getTaskByTitle(@PathVariable String title){
+        Optional<Task> task = taskService.findByTitle(title);
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        //return ResponseEntity.ok(taskService.saveTask(task));
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.saveTask(task));
+    }
+
+    @PutMapping
+    public ResponseEntity<Task> updateTask(@RequestBody Task task){
+        return ResponseEntity.ok(taskService.saveTask(task));
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Task> delete(@PathVariable Long id){
+        Optional<Task> task = taskService.findById(id);
+        if (task.isPresent()) {
+            //Si el id es válido lo borramos y devolvemos 204
+            taskService.deleteTask(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            //Si el id no es de una tarea válida
             return ResponseEntity.notFound().build();
-        else
-            return ResponseEntity.ok(task.get());
-        * */
+        }
+
+    }
+
+    @DeleteMapping("/tasks")
+    public ResponseEntity<Task> deleteAll(){
+        taskService.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/jaroso/apiejemplo2026/entities/Task.java
+++ src/main/java/com/jaroso/apiejemplo2026/entities/Task.java
@@ -1,3 +1,4 @@
+
 package com.jaroso.apiejemplo2026.entities;
 
 import jakarta.persistence.*;
@@ -6,18 +7,23 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Entity(name = "tasks")
-@AllArgsConstructor //estas anotaciones pertenecen a Lombok y automatiza las clases POJO
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Autoincrement
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String title;
+
     private String description;
+
     @Column(name = "finish_date", nullable = false)
     private LocalDate finishDate;
+
 }
